9d189a1e81fd07b69c8a67157957b128e8fe0f1c
jagodlove01-cyber <jagodlove01@gmail.com>
2026-04-19 18:45:30 -0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -10,7 +10,7 @@ from typing import List
 from datetime import datetime, timezone
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
-client = AsyncIOMotorClient(os.environ['MONGO_URL'])
+client = AsyncIOMotorClient(os.environ['MONGO_URL'], tls=True, tlsAllowInvalidCertificates=True)
 db = client[os.environ['DB_NAME']]
 app = FastAPI()
 api_router = APIRouter(prefix="/api")
